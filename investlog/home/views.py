49104b18b0a988a2cd36bf9a76f8ae0055e556be
David Kostadinov <david.kostadinov49@gmail.com>
2024-01-11 16:50:00 +0200

    Added contact form and email sending functionality

--- investlog/home/views.py
+++ investlog/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib.staticfiles.views import serve
+import smtplib
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 def index(request):
     context = {'user': request.user}
@@ -10,5 +15,14 @@ def about(request):
     return render(request, 'home/about.html', context)
 
 def contact(request):
+    if request.method == "POST":
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=os.environ.get("EMAIL_SENDER"), password=os.environ.get("EMAIL_PASSWORD"))
+            connection.sendmail(
+                from_addr=os.environ.get("EMAIL_SENDER"),
+                to_addrs=os.environ.get("EMAIL_RECEIVER"),
+                msg=f"Subject:From {request.POST.get('name')}\n\nEmail:{request.POST.get('email')}\n\n{request.POST.get('message')}",
+            )
     context = {'user': request.user}
     return render(request, 'home/contact.html', context)
