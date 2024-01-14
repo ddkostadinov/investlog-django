b8993871253382a8b0d4f4487c292b8e30a5aee6
David Kostadinov <david.kostadinov49@gmail.com>
2024-01-14 15:28:31 +0200

    Dashboard edit, adding create new investment

--- investlog/dashboard/urls.py
+++ investlog/dashboard/urls.py
@@ -1,7 +1,9 @@
 # dashboard/urls.py
 from django.urls import path
-from .views import Dashboard
+from .views import Overview, Investments, AddInvestment
 
 urlpatterns = [
-    path('', Dashboard.as_view(), name='dashboard'),
+    path('', Overview.as_view(), name='overview'),
+    path('investments/', Investments.as_view(), name='investments'),
+    path('add-investment/', AddInvestment.as_view(), name='add-investment'),
 ]
--- investlog/dashboard/views.py
+++ investlog/dashboard/views.py
@@ -3,8 +3,20 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Dashboard(LoginRequiredMixin, View):
+class Overview(LoginRequiredMixin, View):
 
     def get(self, request):
         context = {'user': request.user}
-        return render(request, 'dashboard/index.html', context=context)
+        return render(request, 'dashboard/overview.html', context=context)
+    
+class Investments(LoginRequiredMixin, View):
+
+    def get(self, request):
+        context = {'user': request.user}
+        return render(request, 'dashboard/investments.html', context=context)
+    
+class AddInvestment(LoginRequiredMixin, View):
+
+    def get(self, request):
+        context = {'user': request.user}
+        return render(request, 'dashboard/add-investment.html', context=context)
