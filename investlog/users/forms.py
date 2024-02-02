bcaedcc7f8b47d5ec522afaba1045430b998cc04
David Kostadinov <david.kostadinov49@gmail.com>
2024-02-03 00:10:08 +0200

    Small change

--- investlog/users/forms.py
+++ investlog/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 class UserRegisterForm(UserCreationForm):
